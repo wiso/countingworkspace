1a3cf1dbc98ed1ae82288db412bcfc3d59554669
Ruggero Turra <ruggero.turra@cern.ch>
2018-11-16 00:07:39 +0100

    add requirement in setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/wiso/countingworkspace",
     packages=setuptools.find_packages(),
+    install_requires=['numpy'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 2",
