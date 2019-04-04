0dd2ab9948362b7b7f09cd261957a68fd02192a1
Ruggero Turra <ruggero.turra@cern.ch>
2019-04-04 14:33:52 +0200

    version 2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="countingworkspace",
-    version="0.1.0",
+    version="0.2.0",
     author="Ruggero Turra",
     author_email="ruggero.turra@cern.ch",
     description="Simple builder for counting experiment RooFit workspace",
